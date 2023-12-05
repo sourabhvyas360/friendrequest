c50475ea050017d72e1fb3310c7de120397ae9fa
Sourabh <sourabhvyas360@gmail.com>
2023-12-05 14:06:18 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,25 @@
+var istatus = document.querySelector("h5");
+
+var btn = document.querySelector("#add");
+
+
+
+var check = 0;
+
+
+btn.addEventListener("click",function(){
+    if(check==0){
+        istatus.innerHTML = "Friends"
+        istatus.style.color = "green"
+        btn.innerHTML = "Remove Friend"
+        check = 1
+    }
+    else{
+        istatus.innerHTML = "Strangers"
+        istatus.style.color = "red"
+        btn.innerHTML = "Add Friends"
+        check = 0
+    }
+    
+});
+
